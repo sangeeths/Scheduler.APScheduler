112bd2e07d4653988bad987dfa79e78af7d1fb9d
Sangeeth Saravanaraj <sangeeth.s@gmail.com>
2013-02-21 05:56:58 +0530

    1. Added basic functions (at, every, after, cron) and its decorators. 2. unschedule, get_scheduled_jobs, get_scheduled_jobs_list, etc. 3. usage.py - example for all the functions.

--- scheduler.py
+++ scheduler.py
@@ -4,48 +4,141 @@ from datetime import datetime, date, time, timedelta
 import time
 import logging
 
+# # # # # # # # # # # # # # # # # # # # # # # # # # # #
+#
+# Logging 
+#
+# __g_log_file      : log file name for the scheduler
+#                     [default: /tmp/scheduler_logs]
+# __g_log_level     : log level for the scheduler
+#                     [default: logging.DEBUG]  
+# __g_log_format    : logging format for the scheduler
+#                     [default: "date & time : level : msg"]
+#
+# TODO: Sechudler.configure() should accept the above
+#       mentioned logging parameters from the user and 
+#       configure the logging.basicConfig accordingly
+#       (or) read these input from a config file.
+#
+__g_log_file   = '/tmp/scheduler_logs'
+__g_log_level  = logging.DEBUG
+__g_log_format = '[%(asctime)s]: %(levelname)s : %(message)s'
+
+# configure logging
+logging.basicConfig(filename=__g_log_file, 
+                    level=__g_log_level, 
+                    format=__g_log_format)
+#
+# End of Logging
+# # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # #
+#
+# Exception Classes
+#
 class SchedulerNotRunningError(Exception):
     """
-    Raised when attempting to stop the scheduler 
+    Raised when attempting to stop the scheduler
     when it is not running.
     """
     def __str__(self):
-        return 'Attempting to stop a Scheduler which is not running'
+        return 'Attempting to stop a Scheduler ' \
+               'which is not running'
+
+class UnSupportedParameter(Exception):
+    """
+    Raised when attempting to pass 
+    an unsupported parameter.
+    """
+    def __str__(self):
+        return 'Attempting to pass ' \
+               'an unsupported parameter.'
+
+class UnSupportedFeature(Exception):
+    """
+    Raised when attempting to invoke a 
+    functionality which is not supported.
+    """
+    def __str__(self):
+        return 'This feature is not ' \
+               'currently supported.'
+#
+# Add more exception classes above!
+#
+# End of Exception Classes
+# # # # # # # # # # # # # # # # # # # # # # # # # # # #
 
 
-logging.basicConfig(filename='/tmp/log', level=logging.DEBUG,
-        format='[%(asctime)s]: %(levelname)s : %(message)s')
 
-
-# Job Store details
-# todo: read these info from file
+# # # # # # # # # # # # # # # # # # # # # # # # # # # #
+#
+# Database Configuration
+#
+# __db_username : username for db [default: root]
+# __db_password : password for db [default: root123]
+# __db_location : ip addr or name [default: localhost]
+# __db_dbname   : name of the db  [default: jobstore]
+#
+# TODO: read these parameters from a config file
+# TODO: update __db_dbname with an appropriate name
+# TODO: get rid of the plain text password
+#
 __db_username = 'root'
 __db_password = 'root123'
 __db_location = 'localhost'
 __db_dbname   = 'jobstore'
 
-# Job Store URL to be passed to scheduler configure
-# url = 'mysql://root:root123@localhost/jobstore'
+#
+# configure the url which will be passed to the 
+# Scheduler.configure to configure the jobstore.
+# url format: 
+#   'mysql://root:root123@localhost/jobstore'
+#
 __db_url = 'mysql://%s:%s@%s/%s' % \
            (__db_username, __db_password, \
             __db_location, __db_dbname)
+#
+# End of Exception Classes
+# # # # # # # # # # # # # # # # # # # # # # # # # # # #
 
-# use 'default' as the name of your job store
-# jbs = Job Store
-__jbs_name = 'default'
 
-# todo: change the table name 
-# jbs_tn = Job Store Table Name 
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # #
+#
+# JobStore and Scheduler Configuration
+# 
+# __jbs = JoBStore
+#
+# __jbs_name    : name of the jobstore 
+#                 [default: "default", this would be
+#                           the default job store]
+# __jbs_tn      : jobstore table name 
+#                 [default: "Framework_Jobs"]
+# __jbs_class   : apscheduler class name for default jobstore
+#                 [default: apscheduler.jobstore.default.class]
+# __jbs_url     : apscheduler class name for default url
+#                 [default: apscheduler.jobstore.default.url]
+# __jbs_table   : apscheduler class name for default table name
+#                 [default: apscheduler.jobstore.default.tablename]
+# __jbs_sqlalchemy : apscheduler class name for SQLAlchemy jobstore
+# [default: apscheduler.jobstores.sqlalchemy_store:SQLAlchemyJobStore]
+# 
+# TODO: update __jbs_tn with appropriate table name
+# TODO: read these parameters from a config file
+#
+__jbs_name  = 'default'
 __jbs_tn    = 'Framework_Jobs'
 __jbs_class = 'apscheduler.jobstore.%s.class' % __jbs_name
 __jbs_url   = 'apscheduler.jobstore.%s.url' % __jbs_name
 __jbs_table = 'apscheduler.jobstore.%s.tablename' % __jbs_name
 __jbs_sqlalchemy = 'apscheduler.jobstores.sqlalchemy_store:SQLAlchemyJobStore'
+
 # APScheduler (preferred) default configuration
-# todo: read these info from a .ini file
-#       from a configurable location
-#
-_g_aps_default_config = {
+# NOTE: do NOT use this config for now. 
+#       for more info, refer readme file.
+# NOTE: currently, persistant job storage is not supported
+_g_aps_default_sql_config = {
     'apscheduler.misfire_grace_time' : 1,
     'apscheduler.coalesce'           : True,
     'apscheduler.daemonic'           : True,
@@ -55,13 +148,37 @@ _g_aps_default_config = {
     __jbs_table : __jbs_tn
 }
 
+# NOTE: use the following config for now (TEMPORARY)
+#       it is RAMJobStore and not SQLAlchemyJobStore
+_g_aps_default_ram_config = {
+    'apscheduler.misfire_grace_time' : 1,
+    'apscheduler.coalesce'           : True,
+    'apscheduler.daemonic'           : True,
+    'apscheduler.standalone'         : True,
+}
+#
+# End of Exception Classes
+# # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # #
+#
+# Scheduler Class
+#
 class Scheduler:
     """
-    Wrapper for APScheduler.
+    A wrapper for APScheduler.
+     
+    This provides functions (and decorators)
+    at(), every(), after() and cron().
     """
 
     # Handle to APScheduler
-    __aps = APScheduler(_g_aps_default_config)
+    # NOTE: do not use SQL config for now; use RAM config.
+    #       (TEMPORARY)
+    # 
+    #__aps = APScheduler(_g_aps_default_sql_config)
+    __aps = APScheduler(_g_aps_default_ram_config)
 
     def __init__(self, aps_config={}):
         self.configure(aps_config) 
@@ -69,17 +186,16 @@ class Scheduler:
     def configure(self, aps_config={}):
         """
         Re-configure the Scheduler with the 
-        user preferred options
+        user preferred parameters, if given.
         """
         if Scheduler.__aps.running: 
             raise SchedulerAlreadyRunningError
-        if aps_config: Scheduler.__aps.configure(aps_config)
+        if aps_config: 
+            Scheduler.__aps.configure(aps_config)
 
     def start(self):
         """
         Start the Scheduler. 
-        Raise SchedulerAlreadyRunningError 
-        if already running.
         """
         if Scheduler.__aps.running: 
             raise SchedulerAlreadyRunningError
@@ -88,8 +204,6 @@ class Scheduler:
     def stop(self):
         """
         Stop the Scheduler.
-        Raise SchedulerNotRunningError
-        if not running.
         """
         if not Scheduler.__aps.running: 
             raise SchedulerNotRunningError
@@ -102,17 +216,48 @@ class Scheduler:
         return Scheduler.__aps.running
 
     def at(self, func, date, args=None, **options):
+        """
+        Schedules a job to be completed
+        _at_ a specific future date and time.
+
+        func : name of the callable function 
+        date : date and time to be called
+        args : arguments to the function 'func'
+        """
         return Scheduler.__aps.add_date_job(func, date, args, options)
 
     def schedule_at(self, date, args=None, **options):
+        """
+        Decorator for Scheduler.at()
+        """
         def decorator(func):
             func.job = Scheduler.__aps.add_date_job(func, date, args, options)
             return func
         return decorator
 
+    # TODO: Scheduler.every() uses APScheduler's add_interval_job()
+    #       which currently does not support scheduling parameters 
+    #       like year and month. Provide a patch to support this!
     def every(self, func, weeks=0, days=0, hours=0, minutes=0, 
               seconds=0, start_date=None, args=None, kwargs=None, 
               **options): 
+        """
+        Schedules a job to be completed for 
+        _every_ specified weeks, days, hours, 
+        minutes, and seconds starting from 
+        the given start date and time.
+
+        func    : name of the callable function
+        weeks   : number of weeks to wait
+        days    : number of days to wait
+        hours   : number of hours to wait
+        minutes : number of minutes to wait
+        seconds : number of seconds to wait
+        start_date: when to first execute the job 
+                    and start the counter (default 
+                    is after the given interval)
+        args    : arguments to the function 'func'
+        """
         return Scheduler.__aps.add_interval_job(func, weeks, days, 
                                                 hours, minutes, seconds, 
                                                 start_date, args, options)
@@ -120,6 +265,9 @@ class Scheduler:
     def schedule_every(self, weeks=0, days=0, hours=0, 
                        minutes=0, seconds=0, start_date=None, 
                        args=None, kwargs=None, **options): 
+        """
+        Decorator for Scheduler.every()
+        """
         def decorator(func):
             func.job = Scheduler.__aps.add_interval_job(func, weeks, days, 
                                                 hours, minutes, seconds, 
@@ -129,6 +277,19 @@ class Scheduler:
 
     def after(self, func, weeks=0, days=0, hours=0, minutes=0, 
               seconds=0, args=None, kwargs=None, **options): 
+        """
+        Schedules a job to be completed _after_ 
+        the specified weeks, days, hours, 
+        minutes and seconds; starting now.
+
+        func    : name of the callable function
+        weeks   : number of weeks to wait
+        days    : number of days to wait
+        hours   : number of hours to wait
+        minutes : number of minutes to wait
+        seconds : number of seconds to wait
+        args    : arguments to the function 'func'
+        """
         alarm_time = datetime.now() + \
                      timedelta(weeks=weeks, days=days, 
                                hours=hours, minutes=minutes, 
@@ -137,6 +298,9 @@ class Scheduler:
 
     def schedule_after(self, weeks=0, days=0, hours=0, minutes=0, 
                        seconds=0, args=None, kwargs=None, **options): 
+        """
+        Decorator for Scheduler.after()
+        """
         def decorator(func):
             alarm_time = datetime.now() + timedelta(weeks=weeks, days=days, 
                          hours=hours, minutes=minutes, seconds=seconds)
@@ -148,6 +312,24 @@ class Scheduler:
              week='*', day_of_week='*', hour='*', 
              minute='*', second='*', start_date=None, 
              args=None, kwargs=None, **options):
+        """
+        Schedules a job to be completed on times
+        the matches the given expression.
+
+        func    : name of the callable function
+        year    : year to run on
+        month   : month to run on
+        day     : day of month to run on
+        week    : week of the year to run on
+        day_of_week: weekday to run on (0 = Monday)
+        hour    : hour to run on
+        minute  : number of minutes to wait
+        second  : second to run on
+        start_date: when to first execute the job 
+                    and start the counter (default 
+                    is after the given interval)
+        args    : arguments to the function 'func'
+        """
         return Scheduler.__aps.add_cron_job(func, year, month, day, week, 
                                             day_of_week, hour, minute, second, 
                                             start_date, args, options)
@@ -156,6 +338,9 @@ class Scheduler:
                       week='*', day_of_week='*', hour='*', 
                       minute='*', second='*', start_date=None, 
                       args=None, kwargs=None, **options):
+        """
+        Decorator for Scheduler.cron()
+        """
         def decorator(func):
             func.job = Scheduler.__aps.add_cron_job(func, year, month, day, week, 
                                             day_of_week, hour, minute, second, 
@@ -163,4 +348,70 @@ class Scheduler:
             return func
         return decorator
 
+    def unschedule(self, job=None, func=None, jobid=None):
+        """
+        Unschedule a given job or function or jobid.
+        """
+        if job is None and func is None and jobid is None:
+            # future compatibility: 
+            # TODO: unschedule all the jobs and functions
+            #       but for now, just raise an exception
+            raise UnSupportedFeature
+        elif job is None and func is not None and jobid is None:
+            # unschedule the given function in 'func' parameter
+            # ignore 'job' and 'jobid' parameters
+            Scheduler.__aps.unschedule_func(func)
+        elif job is not None and func is None and jobid is None:
+            # unschedule the given job in 'job' parameter
+            # ignore 'func' and 'jobid' parameters
+            Scheduler.__aps.unschedule_job(job)
+        elif job is None and func is None and jobid is not None:
+            # unschedule the given job in 'jobid' parameter
+            # ignore 'func' and 'job' parameters
+            # TODO: this feature is expected in APScheduler 3.0
+            #       placeholder till then!
+            raise UnSupportedFeature
+        else:
+            # wrong incoming parameter combinations 
+            raise UnSupportedParameter
+            
+    def get_scheduled_jobs(self):
+        """
+        Returns a list of all scheduled jobs. 
+        """
+        return Scheduler.__aps.get_jobs()
 
+    def get_scheduled_jobs_list(self):
+        """
+        Prints the list of all scheduled jobs 
+        in human readable format. 
+        """
+        return Scheduler.__aps.print_jobs()
+
+#
+# End of Scheduler Classes
+# # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # #
+#
+# Print Help Message when this file is executed
+#
+if __name__ == '__main__':
+    print """    This scheduler provides the following functions 
+     [1] at()    : execute func at given date & time 
+     [2] every() : execute func every w, d, h, s, m 
+     [3] after() : execute func after given date & time 
+     [4] cron()  : execute func based on various expressions 
+                   on each field - y, m, d, w, dow, h, m, s 
+
+    NOTE:  
+     [a] the scheduler provides decorators  
+         for all the above mentioned functions. 
+     [b] please refer the usage.py for examples
+         on how to use the scheduler.
+
+    Thank you!!"""
+#
+# End 
+# # # # # # # # # # # # # # # # # # # # # # # # # # # #
---
+++ usage.py
@@ -0,0 +1,262 @@
+# import the scheduler
+from scheduler import Scheduler as scheduler
+
+# to calculate date and time 
+from datetime import datetime, timedelta
+
+# inspect is needed to get the function name
+import inspect
+
+# this is a common function which is scheduled 
+# at many times using many different scheduler 
+# functions like at(), every(), after() and cron(). 
+def func(args):
+    fname = inspect.stack()[0][3] #function name
+    print '%s : %s : current time "%s"' % \
+          (fname, args, datetime.now())
+
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # #
+#
+# create an instance of the scheduler 
+sched = scheduler()
+
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # #
+#
+# Scheduler.at() 
+#
+# input:
+#   func : name of the function to be called
+#   date : date and time to be called
+#   args : arguments to the function 'func'
+#
+# an at() job executes at 10 seconds from now
+j1_time = datetime.now() + timedelta(seconds=10)
+at_j1 = sched.at(func, j1_time, args=["AT +10s"])
+
+# an at() job executes at 20 seconds from now
+j2_time = datetime.now() + timedelta(seconds=20)
+at_j2 = sched.at(func, j2_time, args=["AT +20s"])
+
+# an at() job executes at 30 seconds from now
+j3_time = datetime.now() + timedelta(seconds=30)
+at_j3 = sched.at(func, j3_time, args=["AT +30s"])
+
+# decorator for at() 
+# executes at 15 seconds from now
+j4_time = datetime.now() + timedelta(seconds=15)
+@sched.schedule_at(date=j4_time, 
+                   args=["DECORATOR", "AT", "+15s"])
+def dec_at(*args):
+    fname = inspect.stack()[0][3] #function name
+    print '%s : %s : current time "%s"' % \
+          (fname, args, datetime.now())
+#
+# End of at() 
+# # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # #
+#
+# Scheduler.every() 
+#
+# input:
+#   func    : name of the function to be called
+#   weeks   : number of weeks to wait
+#   days    : number of days to wait
+#   hours   : number of hours to wait
+#   minutes : number of minutes to wait
+#   seconds : number of seconds to wait
+#   start_date: when to first execute the job 
+#               and start the counter 
+#               (default is after the given interval)
+#   args    : arguments to the function 'func'
+#
+# executes every 5 seconds starting now 
+every_j1 = sched.every(func, args=["EVERY 5s"], seconds=5)
+
+# executes every 10 seconds 
+# starting at 90 seconds from now
+j2_start_date = datetime.now() + timedelta(seconds=90)
+every_j2 = sched.every(func, start_date=j2_start_date, 
+                       args=["EVERY 10s"], seconds=10)
+
+# decorator for every()
+# executes every 30 seconds starting now
+@sched.schedule_every(seconds=30,
+                      args=["DECORATOR", "EVERY", "+30s"])
+def dec_every(*args):
+    fname = inspect.stack()[0][3] #function name
+    print '%s : %s : current time "%s"' % \
+          (fname, args, datetime.now())
+#
+# End of every() 
+# # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # #
+#
+# Scheduler.after() 
+#
+# input:
+#   func    : name of the function to be called
+#   weeks   : number of weeks to wait
+#   days    : number of days to wait
+#   hours   : number of hours to wait
+#   minutes : number of minutes to wait
+#   seconds : number of seconds to wait
+#   args    : arguments to the function 'func'
+#
+# executes after 45 seconds from now
+after_j1 = sched.after(func, seconds=45, 
+                       args=["AFTER +45s"])
+
+# executes after 2 minutes 30 seconds from now
+j7 = sched.after(func, minutes=2, seconds=30, 
+                 args=["AFTER +2m30s"])
+
+#decorator for "after"
+@sched.schedule_after(minutes=1, seconds=2,
+                      args=["DECORATOR", "AFTER", "+1m2s"])
+def dec_after(*args):
+    fname = inspect.stack()[0][3] #function name
+    print '%s : %s : current time "%s"' % \
+          (fname, args, datetime.now())
+#
+# End of after() 
+# # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # #
+#
+# Scheduler.cron() 
+#
+# input:
+#   func    : name of the function to be called
+#   year    : year to run on
+#   month   : month to run on
+#   day     : day of month to run on
+#   week    : week of the year to run on
+#   day_of_week: weekday to run on (0 = Monday)
+#   hour    : hour to run on
+#   minute  : number of minutes to wait
+#   second  : second to run on
+#   start_date: when to first execute the job 
+#               and start the counter 
+#               (default is after the given interval)
+#   args    : arguments to the function 'func'
+#
+# executes at the following:
+# year      : 2012, 2013, 2014 and 2015
+# months    : jan, feb, march, may, july, sept, nov and dec
+# seconds   : every 5 seconds  
+cron_j1 = sched.cron(func, year='2012-2015', 
+                     month='1-3,5,7,9,11-12', 
+                     second='5,10,15,20,25,30,35,40,45,50,55,60', 
+                     args=["CORN 5s,m1-3,5,7,9,11,12,y2012-15"])
+
+# decorator for corn()
+# executes at the following:
+# year      : 2013
+# months    : feb, march, apr, may, june and july
+# seconds   : every 15 seconds  
+@sched.schedule_cron(year='2013', month='2-7', second='15,30,45,60',
+                     args=["DECORATOR", "CORN", "15s,m2-7,y2013"])
+def dec_cron(*args):
+    fname = inspect.stack()[0][3] #function name
+    print '%s : %s : current time "%s"' % \
+          (fname, args, datetime.now())
+#
+# End of cron() 
+# # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # #
+#
+# Scheduler.get_scheduled_jobs() 
+# Scheduler.get_scheduled_jobs_list() 
+#
+# return:
+#   get_scheduled_jobs : list of all scheduled jobs
+#                        each entry on this list are 
+#                        instance of type job 
+#
+#   get_scheduled_jobs_list : human readable list of 
+#                             all scheduled jobs,
+#                             returns string.
+#
+# print the list of all scheduled jobs 
+# for every 1 minute
+@sched.schedule_every(minutes=1,
+                      args=["EVERY", "+1m"])
+def dec_print_scheduled_jobs(*args):
+    fname = inspect.stack()[0][3] #function name
+    print '%s : %s : current time "%s"' % \
+          (fname, sched.get_scheduled_jobs(), \
+          datetime.now())
+
+# print the list of all scheduled jobs in 
+# human readable format - for every 45 seconds
+@sched.schedule_every(seconds=45,
+                      args=["EVERY", "+45s"])
+def dec_print_scheduled_jobs_list(*args):
+    fname = inspect.stack()[0][3] #function name
+    print '%s : %s : current time "%s"' % \
+          (fname, sched.get_scheduled_jobs_list(), \
+          datetime.now())
+#
+# End of Scheduler.get_scheduled_jobs()
+# End of Scheduler.get_scheduled_jobs_list()
+# # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # #
+#
+# Scheduler.unschedule() 
+#
+# input:
+#   job     : instance of type job to be unscheduled 
+#   func    : function - all jobs scheduled on this 
+#             function will be unscheduled
+#   jobid   : job with jobid will be unscheduled (TODO)
+#
+# unschedule_func function is called at 3 minutes 
+# from now. this function will unschedule all the 
+# above scheduled example functions!
+unsched_time = datetime.now() + timedelta(minutes=3)
+@sched.schedule_at(date=unsched_time, 
+                   args=["UNSCHEDULE", "AT", "+3m"])
+def unschedule_func(*args):
+    fname = inspect.stack()[0][3] #function name
+    print '%s : %s : current time "%s"' % \
+          (fname, args, datetime.now())
+    #sched.unschedule(job=j4)
+    #sched.unschedule(jobid=123)
+    sched.unschedule(func=func)
+    sched.unschedule(func=dec_cron)
+    #sched.unschedule(func=dec_after)
+    sched.unschedule(func=dec_every)
+    #sched.unschedule(func=dec_at)
+    sched.unschedule(func=dec_print_scheduled_jobs)
+    sched.unschedule(func=dec_print_scheduled_jobs_list)
+#
+# End of Scheduler.unschedule()
+# # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # #
+#
+# start the scheduler
+#
+print 'This program will run for 3 minutes'
+print 'To exit before that, press Ctrl+C'
+sched.start()
+
+
+print '__END__'
+
